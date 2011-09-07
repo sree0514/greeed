7154b3466e46bf69174cbdc259c2400f8112c357
Sreehari <sree0514@gmail.com>
2011-09-07 23:42:03 +0530

    fourth

--- lib/player.rb
+++ lib/player.rb
@@ -1,30 +1,35 @@
 class Player
   attr_accessor :count
   attr_accessor :points
+  MAX_REPEATS = 3
   def initialize 
     @count = 0
     @points = 0
   end
+  
   def calculate_points(value, iteration)
     @previous_value = value if iteration == 0
-    if @previous_value == value || iteratin == 0 || value == 1 || value == 5
+    if @previous_value == value || value == 1 || value == 5
       @count += 1
-      case value
-        when 1
-          @points += 100
-          @points += 700 if @count == 3
-        when 2
-          @points += 200 if @count == 3
-        when 3
-          @points += 300 if @count == 3
-        when 4
-          @points += 400 if @count == 3
-        when 5
-          @points += 50
-          @points += 350 if @count == 3
-        when 6 
-          @points += 600 if @count == 3
-        end 
-     end
+      if value == 1
+        @points += 100
+        @points += 700 if @count == MAX_REPEATS
+      end
+      @points += 200 if(@count == MAX_REPEATS && value == 2)
+      @points += 300 if(@count == MAX_REPEATS && value == 3)
+      @points += 400 if(@count == MAX_REPEATS && value == 4)
+      if value == 5
+        @points += 50
+        @points += 350 if @count === MAX_REPEATS
+      end 
+      @points += 600 if(@count == MAX_REPEATS && value == 6)
     end
+    @previous_value = value
+  end 
+   
 end
+@player=Player.new
+#@player.calculate_points(1,1)
+#puts @player.calculate_points(1,2)
+ 3.times { |i| @player.calculate_points(3,i)}
+  puts @player.points
